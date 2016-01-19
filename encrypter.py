9afd5f72b32bbaf8c54290be60a16495d628004e
dairvin9 <dairvin9@tamu.edu>
2016-01-18 22:40:59 -0600

    updated encrypter, unfinished

--- encrypter.py
+++ encrypter.py
@@ -19,7 +19,7 @@ formatNumber = str((int(formatNumber) * random.randrange(2,777))% 1000) #increas
 #Converts the file to binary representation, which is 7 bits long
 fileContents = ''.join(format(ord(x)+int(formatNumber), 'b') for x in fileContents)
 
-file.write(''.join(format(ord(x), 'b') for x in formatNumber))
-file.write(fileContents)
+#file.write(''.join(format(ord(x), 'b') for x in formatNumber))
+
  
 file.close()        
